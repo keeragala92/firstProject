accd78870da53618481a2b3370c8bd61c1d5d8f2
Isuru Sampath <kasamee92@gmail.com>
2022-10-02 14:10:49 +0900

    initial commit

---
+++ src/HomeCC.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {BackHandler, ImageBackground, Text, TouchableOpacity, View} from 'react-native';
+
+import styles from './HomeCC.styles';
+
+class HomeCC extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onIncrease() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  onDecrease() {
+    this.setState({
+      count: (this.state.count = 1),
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{this.state.count}</Text>
+
+        <view style={{flexDirection:'row' Bac}}>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => this.onIncrease()}>
+            <Text style={styles.btnTitle}>+ Button</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btn2}
+            onPress={() => this.onDecrease()}>
+            <Text style={styles.btnTitle}>- Button</Text>
+          </TouchableOpacity>
+        </view>
+      </View>
+    );
+  }
+}
+
+export default HomeCC;
---
+++ src/HomeCC.styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 26,
+    color: '#000000',
+    padding: 10,
+  },
+
+  btn: {
+    backgroundColor: '#e01010',
+    padding: 22,
+  },
+
+  btn:2 {
+    backgroundColor: '#e01010',
+    padding: 22,
+    
+  }
+
+  btnTitle: {
+    fontSize: 26,
+    color: '#FFFFFF',
+  },
+});
+
+export default styles;
